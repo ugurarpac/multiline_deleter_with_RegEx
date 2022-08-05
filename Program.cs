21ad6e90d4b364ac9c2fc914beb61cb062b547f7
ugurarpac <73073617+ugurarpac@users.noreply.github.com>
2022-08-05 09:56:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+public class Program
+{
+    public static void Main()
+    {
+        string subjectString = @"Hello1
+
+Hello2
+
+/*Hello3 
+
+Hello4*/
+
+         Hello5
+
+/*/if (!IsPostBack)
+            //{
+            //*/    //binddummyDT();
+               
+            //};
+            ";
+        Regex comments2 = new Regex(@"/\*(.|\n)*?\*/");
+        string afterRemoval = comments2.Replace(subjectString, "");
+        Console.WriteLine(afterRemoval);
+
+    }
+}
